86e8d56b512af3f09fa6ef399db9790bc44bf90f
Quinten De Man <quinten.deman@stonybrook.edu>
2021-06-14 18:38:47 -0400

    added dequeue and peek functionality

--- Queue.js
+++ Queue.js
@@ -18,32 +18,45 @@ class Queue {
 }
 
 // initialize a Queue to be used by the page
-mainQueue = new Queue();
-mainQueue.enqueue(1);	//temporary
-mainQueue.enqueue(2);	//temporary
+var mainQueue = new Queue()
+mainQueue.enqueue(1)	//temporary
+mainQueue.enqueue(2)	//temporary
+var lastReturned = undefined
+// get the canvas and canvas context
+var canvas = document.getElementById("mainCanvas")
+var ctx = canvas.getContext("2d")
+// get the queue input and output box components
+var queueInput = document.getElementById("queueInput")
+var queueOutput = document.getElementById("queueOutput")
 
 //function to refresh the canvas after any Queue operation
 function refreshCanvas() {
+	ctx.clearRect(0,0,canvas.width,canvas.height)
 	
 }
 // function to add to mainQueue
 function mainEnqueue() {
-	input = document.getElementById("enqueueInput");
-	data = parseInt(input.value);
+	var data = parseInt(queueInput.value)
 	if (!isNaN(data))
-		mainQueue.enqueue(data);
-	input.value = null;
-	refreshCanvas();
+		mainQueue.enqueue(data)
+	queueInput.value = null
+	refreshCanvas()
 }
 // function to remove from mainQueue
 function mainDequeue() {
-	data = mainQueue.dequeue();
-	
-	refreshCanvas();
+	lastReturned = mainQueue.dequeue()
+	if(lastReturned == undefined)
+		queueOutput.value = "None"
+	else
+		queueOutput.value = lastReturned
+	refreshCanvas()
 }
 // function to peek from mainQueue
 function mainPeek() {
-	data = mainQueue.peek();
-	
-	refreshCanvas();
+	lastReturned = mainQueue.peek()
+	if(lastReturned == undefined)
+		queueOutput.value = "None"
+	else
+		queueOutput.value = lastReturned
+	refreshCanvas()
 }
