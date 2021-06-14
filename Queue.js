45877e62abb43a6ae91fe1c59b19b21910ed3f23
Quinten De Man <quinten.deman@stonybrook.edu>
2021-06-14 19:54:19 -0400

    added queue animations

--- Queue.js
+++ Queue.js
@@ -29,11 +29,45 @@ var ctx = canvas.getContext("2d")
 var queueInput = document.getElementById("queueInput")
 var queueOutput = document.getElementById("queueOutput")
 
-//function to refresh the canvas after any Queue operation
+// draw a left arrow with the point at (x,y)
+function drawLeftArrow(x, y, width, height) {
+	ctx.beginPath()
+	ctx.moveTo(x,y)
+	ctx.lineTo(x+width/3,y+height/2)
+	ctx.lineTo(x+width/3,y+height/4)
+	ctx.lineTo(x+width,y+height/4)
+	ctx.lineTo(x+width,y-height/4)
+	ctx.lineTo(x+width/3,y-height/4)
+	ctx.lineTo(x+width/3,y-height/2)
+	ctx.lineTo(x,y)
+	ctx.lineWidth = 2
+	ctx.stroke()
+	ctx.fill()
+}
+//functions to redraw the canvas after any Queue operation
 function refreshCanvas() {
 	ctx.clearRect(0,0,canvas.width,canvas.height)
-	
+	ctx.font = "bold 16px monospace"
+	ctx.textAlign = "start"
+	ctx.fillStyle = "#FFCCCB"
+	drawLeftArrow(15, canvas.height/2, 75, 50)
+	ctx.fillStyle = "#90EE90"
+	drawLeftArrow(mainQueue.array.length*100+110, canvas.height/2, 75, 50)
+	ctx.fillStyle = "#000000"
+	ctx.fillText("Dequeue", 26, canvas.height/2+4)
+	ctx.fillText("Enqueue", mainQueue.array.length*100+121, canvas.height/2+4)
+	ctx.lineWidth = 5
+	ctx.font = "bold 25px monospace"
+	ctx.textAlign = "center"
+	for (var i = 0; i < mainQueue.array.length; i++) {
+		ctx.beginPath()
+		ctx.arc(i*100+150, canvas.height/2, 40, 0, 2*Math.PI)
+		ctx.stroke()
+		ctx.fillText(mainQueue.array[mainQueue.array.length-i-1], i*100+148, canvas.height/2+8)
+	}
 }
+//load the canvas in the beginning
+refreshCanvas()
 // function to add to mainQueue
 function mainEnqueue() {
 	var data = parseInt(queueInput.value)
