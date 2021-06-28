1b6ebec76e199564a356d8fd776a3f6a1bd207da
Quinten De Man <quinten.deman@stonybrook.edu>
2021-06-28 16:50:56 -0400

    added list functionality

--- ReactApp/src/List/List.js
+++ ReactApp/src/List/List.js
@@ -7,37 +7,78 @@ const List = () => {
 
     const [, forceRender] = useState(0);
     const [list,] = useState([]);
+    const insertIndex = useRef();
+    const insertValue = useRef();
+    const removeIndex = useRef();
+    const listOutput = useRef();
+    const getIndex = useRef();
 
     //We call update on fake state variable to force rerender
 	const forceUpdate = () => {
 		forceRender(renders => renders+1);
 	}
 
+    const insert = () => {
+        const index = parseInt(insertIndex.current.value);
+        const value = parseInt(insertValue.current.value);
+        if (!isNaN(index) && !isNaN(value) && index >= 0 && index <= list.length) {
+            list.splice(index, 0, value);
+            forceUpdate();
+        } else {
+            listOutput.current.value = "Invalid";
+        }
+        insertIndex.current.value = null;
+        insertValue.current.value = null;
+    }
+
+    const remove = () => {
+        const index = parseInt(removeIndex.current.value);
+        if (!isNaN(index) && index >= 0 && index < list.length) {
+            const data = list.splice(index, 1);
+            forceUpdate();
+            listOutput.current.value = data;
+        } else {
+            listOutput.current.value = "Invalid";
+        }
+        removeIndex.current.value = null;
+    }
+
+    const get = () => {
+        const index = parseInt(getIndex.current.value);
+        if (!isNaN(index) && index >= 0 && index < list.length) {
+            const data = list[index];
+            listOutput.current.value = data;
+        } else {
+            listOutput.current.value = "Invalid";
+        }
+        getIndex.current.value = null;
+    }
+
     return (
         <div className="list">
             <div className="controls">
-                <button id="insertButton">Insert</button>
+                <button id="insertButton" onClick={insert}>Insert</button>
                 <span className="labeledInput">
                     <label>Index</label>
-                    <input id="insertIndex" type="text"></input>
+                    <input id="insertIndex" ref={insertIndex} type="text"></input>
                 </span>
                 <span className="labeledInput">
                     <label>Value</label>
-                    <input id="insertValue" type="text"></input>
+                    <input id="insertValue" ref={insertValue} type="text"></input>
                 </span>
                 <br />
-                <button id="removeButton">Remove</button>
+                <button id="removeButton" onClick={remove}>Remove</button>
                 <span className="labeledInput">
                     <label>Index</label>
-                    <input id="removeIndex" type="text"></input>
+                    <input id="removeIndex" ref={removeIndex} type="text"></input>
                 </span>
                 <br />
-                <input id="listOutput" type="text" readOnly></input>
+                <input id="listOutput" type="text" ref={listOutput} readOnly></input>
                 <br />
-                <button id="getButton">Get</button>
+                <button id="getButton" onClick={get}>Get</button>
                 <span className="labeledInput">
                     <label>Index</label>
-                    <input id="getIndex" type="text"></input>
+                    <input id="getIndex" ref={getIndex} type="text"></input>
                 </span>
             </div>
             <div className="visualization">
