74f44b4ea72d0b316b6294f2d82d6adffe028e3d
Quinten De Man <quinten.deman@stonybrook.edu>
2021-07-03 12:44:02 -0400

    added random array creation, animation speed slider, and legend for selection sort

--- ReactApp/src/SelectionSort/SelectionSort.js
+++ ReactApp/src/SelectionSort/SelectionSort.js
@@ -19,22 +19,51 @@ const SelectionSortDisplay = (props) => {
 const SelectionSort = () => {
 
     const [, forceRender] = useState(0);
-    const [array,] = useState([3,7,2,-3,0,22,5,1,-8,9]);
+    const [array, setArray] = useState([]);
+    const [sorted, setSorted] = useState(false);
     const sorting = useRef(false);
     const interval = useRef(null);
     const sortedEndIndex = useRef(0);
     const currentFocus = useRef(-1);
     const currentMinimum = useRef(-1);
     const toggleSortingButton = useRef();
+    const generateArrayButton = useRef();
+    const arraySizeInput = useRef();
+    const speedSlider = useRef();
 
     //We call update on fake state variable to force rerender
 	const forceUpdate = () => {
 		forceRender(renders => renders+1);
 	}
 
+    //initializes a random array for sorting
+    const generateArray = () => {
+        if (sorting.current) {
+            toggleSorting();
+        }
+        const size = parseInt(arraySizeInput.current.value);
+        if (!isNaN(size) && size > 0) {
+            var newArray = new Array(size);
+            for (let i = 0; i < size; i++) {
+                newArray[i] = randInt(-999,1000);
+            }
+            setArray(newArray);
+            setSorted(false);
+            sortedEndIndex.current = 0;
+            currentFocus.current = -1;
+            currentMinimum.current = -1;
+        }
+        arraySizeInput.current.value = null;
+    }
+    const randInt = (min, max) => {
+        return Math.floor(Math.random() * (max-min) + min);
+    }
+
     //completes one step of the sorting algorithm
     const sortingStep = () => {
         if (sortedEndIndex.current === array.length) {
+            setSorted(true);
+            toggleSorting();
             return;
         }
         if (currentFocus.current === array.length-1) {
@@ -59,24 +88,48 @@ const SelectionSort = () => {
         if (sorting.current) {
             clearInterval(interval.current);
             sorting.current = false;
-            toggleSortingButton.current.innerHTML = "Start";
-        } else {
+            toggleSortingButton.current.innerHTML = "Sort";
+            toggleSortingButton.current.classList.remove("pinkButton");
+            toggleSortingButton.current.classList.add("greenButton");
+        } else if (!sorted) {
             interval.current = setInterval(() => {
                 sortingStep();
                 forceUpdate();
-            }, 100);
+            }, 1000-speedSlider.current.value);
             sorting.current = true;
             toggleSortingButton.current.innerHTML = "Stop";
+            toggleSortingButton.current.classList.remove("greenButton");
+            toggleSortingButton.current.classList.add("pinkButton");
         }
     }
 
     return (
         <div className="selection-sort">
-            <div className="controls">
-                <button ref={toggleSortingButton} onClick={toggleSorting}>Start</button>
+            <div id="main">
+                <div className="controls">
+                    <button id="generateArrayButton" ref={generateArrayButton} onClick={generateArray}>Random</button>
+                    <input id="arraySizeInput" ref={arraySizeInput} type="text"></input>
+                    <br />
+                    <button id="toggleSortingButton" className="greenButton" ref={toggleSortingButton} onClick={toggleSorting}>Sort</button>
+                    <br />
+                    <input className="slider" ref={speedSlider} min="0" max="990" type="range"></input>
+                </div>
+                <div className="visualization">
+                    <SelectionSortDisplay array={array} sortedEndIndex={sortedEndIndex.current} currentFocus={currentFocus.current} currentMinimum={currentMinimum.current} />
+                </div>
             </div>
-            <div className="visualization">
-                <SelectionSortDisplay array={array} sortedEndIndex={sortedEndIndex.current} currentFocus = {currentFocus.current} currentMinimum={currentMinimum.current} />
+            <div id="legend">
+                <Element color="green"></Element>
+                <p>= Sorted</p>
+                < br />
+                <Element></Element>
+                <p>= Unsorted</p>
+                <br />
+                <Element color="pink"></Element>
+                <p>= Current Minimum</p>
+                <br />
+                <Element border="bordered"></Element>
+                <p>= Current Focus</p>
             </div>
         </div>
     );
