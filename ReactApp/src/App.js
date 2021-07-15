f5e85a7aa45195545a6ea0e6daa8998b466721f4
Quinten De Man <quinten.deman@stonybrook.edu>
2021-07-15 19:56:00 -0400

    made BST information section

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -11,6 +11,7 @@ import QueueInformation from './Queue/QueueInformation';
 import BinaryTree from './BinaryTree/BinaryTree';
 import BinaryTreeInformation from './BinaryTree/BinaryTreeInformation';
 import BinarySearchTree from './BinarySearchTree/BinarySearchTree';
+import BinarySearchTreeInformation from './BinarySearchTree/BinarySearchTreeInformation';
 import SelectionSort from './SelectionSort/SelectionSort';
 import LinkedList from './LinkedList/LinkedList';
 import LinkedListInformation from './LinkedList/LinkedListInformation';
@@ -31,68 +32,69 @@ const App = () => {
 					<div className="main">
 						<Navigation />
 						<div className="content">
-						<Switch>
-							<Route exact path="/">
-								<div className="information">
-									<p>Home page</p>
-								</div>
-							</Route>
-							<Route exact path="/List">
-								<ListInformation />
-								<List />
-							</Route>
-							<Route exact path="/Stack">
-								<StackInformation />
-								<Stack />	
-							</Route>
-							<Route exact path="/Queue">
-								<QueueInformation />
-								<Queue />
-							</Route>
-							<Route exact path="/Array">
-								<ArrayInformation />
-								<Array />
-							</Route>
-							<Route exact path="/Linked List">
-								<LinkedListInformation/>
-								<LinkedList />
-							</Route>
-							<Route exact path="/Array List">
-								<ArrayListInformation />
-								<ArrayList />
-							</Route>
-							<Route exact path="/Binary Tree">
-								<BinaryTreeInformation />
-								<BinaryTree />
-							</Route>
-							<Route exact path="/Binary Search Tree">
-								<BinarySearchTree />
-							</Route>
-							<Route exact path="/Graph">
+							<Switch>
+								<Route exact path="/">
+									<div className="information">
+										<p>Home page</p>
+									</div>
+								</Route>
+								<Route exact path="/List">
+									<ListInformation />
+									<List />
+								</Route>
+								<Route exact path="/Stack">
+									<StackInformation />
+									<Stack />
+								</Route>
+								<Route exact path="/Queue">
+									<QueueInformation />
+									<Queue />
+								</Route>
+								<Route exact path="/Array">
+									<ArrayInformation />
+									<Array />
+								</Route>
+								<Route exact path="/Linked List">
+									<LinkedListInformation />
+									<LinkedList />
+								</Route>
+								<Route exact path="/Array List">
+									<ArrayListInformation />
+									<ArrayList />
+								</Route>
+								<Route exact path="/Binary Tree">
+									<BinaryTreeInformation />
+									<BinaryTree />
+								</Route>
+								<Route exact path="/Binary Search Tree">
+									<BinarySearchTreeInformation />
+									<BinarySearchTree />
+								</Route>
+								<Route exact path="/Graph">
 
-							</Route>
-							<Route exact path="/Set">
+								</Route>
+								<Route exact path="/Set">
 
-							</Route>
-							<Route exact path="/Map">
-								
-							</Route>
-							<Route exact path="/Selection Sort">
-								<SelectionSort />
-							</Route>
-							<Route exact path="/Insertion Sort">
-								<InsertionSort />
-							</Route>
-							<Route exact path="/Bubble Sort">
-								<BubbleSort />
-							</Route>
-							<Route exact path="/Merge Sort">
-								<MergeSort />
-							</Route>
-							<Route exact path="/Quick Sort">
-								<QuickSort />
-							</Route>
-						</Switch>
+								</Route>
+								<Route exact path="/Map">
+
+								</Route>
+								<Route exact path="/Selection Sort">
+									<SelectionSort />
+								</Route>
+								<Route exact path="/Insertion Sort">
+									<InsertionSort />
+								</Route>
+								<Route exact path="/Bubble Sort">
+									<BubbleSort />
+								</Route>
+								<Route exact path="/Merge Sort">
+									<MergeSort />
+								</Route>
+								<Route exact path="/Quick Sort">
+									<QuickSort />
+								</Route>
+							</Switch>
 						</div>
 					</div>
 					<footer>Made by Quinten De Man and Zackary Lassetter</footer>
