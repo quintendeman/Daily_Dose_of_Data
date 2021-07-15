d280d3b135fcd0412f0256fd64e2c8620b951a2a
Quinten De Man <quinten.deman@stonybrook.edu>
2021-07-15 17:02:10 -0400

    reorganizing git repo

---
+++ ReactApp/src/Array/ArrayInformation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArrayInformation = () => {
+    return (
+        <div className="information">
+            <h1>Array</h1>
+        </div>
+    );
+}
+
+export default ArrayInformation;
