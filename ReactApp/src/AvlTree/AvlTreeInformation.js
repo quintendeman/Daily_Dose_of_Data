ee0323e1b4d06af1b50a07e04d6b74bd2eef5d2e
zlassetter <zackary@lassetter.org>
2021-08-04 17:24:50 -0400

    edited avl tree info section

--- ReactApp/src/AvlTree/AvlTreeInformation.js
+++ ReactApp/src/AvlTree/AvlTreeInformation.js
@@ -14,6 +14,10 @@ const AvlTreeInformation = () => {
                 <li><b>Find:</b> searches for an element in the tree</li>
             </ul>
             <br />
+            <p>At insertion, a regular BST insertion is performed. Next, if there is an unbalanced node found on the path up from the newly inserted node to the root, then a rotation will occur. The exact rotation operation depeneds on the relative postions of the first unbalanced node and its two immediate adjacent ancestors on the path back to the newly inserted node.</p>
+            <br />
+            <p>Likewise, at deletion, a regular BST deletion is performed and a rotation may also occur. However, whether or not the rotation occurs and the exact rotation operation depends instead on the relative positions of the first unbalanced node on the path up the tree as well as its two immediate ancestors that are each the largest height child of their parent node.</p>
+            <br />
             <p>As a consequence of an AVL Tree maintaining its balance. It accels over a traditional Binary Search Tree by improving the worst case time complexities for each basic operation </p>
             <br />
             <table>
