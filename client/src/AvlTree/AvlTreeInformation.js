5aabe16ba5ac2f9ea16776a4afc4b4cc5f2d5270
zlassetter <zackary@lassetter.org>
2021-08-10 17:13:20 -0400

    Finished Avl Tree Info

--- client/src/AvlTree/AvlTreeInformation.js
+++ client/src/AvlTree/AvlTreeInformation.js
@@ -46,6 +46,26 @@ const AvlTreeInformation = () => {
     rlroot.right = rly;
     rly.left = rlx;
     //var rly = new Binar 
+
+    var leftright = new AvlTreeClass();
+    var lrroot = new BinaryTreeNode("z");
+    var lry = new BinaryTreeNode("y");
+    var lrx = new BinaryTreeNode("x");
+    leftright.root = lrroot;
+    lrroot.left = lry;
+    lry.right = lrx;
+
+    var rrotation = new AvlTreeClass();
+    var rrrotroot = new BinaryTreeNode("y");
+    rrrotroot.left = new BinaryTreeNode("x");
+    rrrotroot.right = new BinaryTreeNode("z");
+    rrotation.root = rrrotroot;
+
+    var lrotation = new AvlTreeClass();
+    var lrotroot = new BinaryTreeNode("y");
+    lrotroot.left = new BinaryTreeNode("z");
+    lrotroot.right = new BinaryTreeNode("x");
+    lrotation.root = lrotroot;
     
     return (
         <div className="information">
@@ -65,12 +85,52 @@ const AvlTreeInformation = () => {
             <br />
             <p>Because of the fact that AVL Trees maintain a balanced state, they have advantages over a traditional Binary Search Tree. The height of the tree will always be O(log n), where as a regular BST may have a height of O(n). The worst-case time complexities for each basic operation are O(log n) in an AVL Tree rather than O(n).</p>
             <br />
-            <BinaryTreeDisplay style={{transform: "translate(100px, 100px)"}} tree={leftleft} />
-            <p>Left Left</p>
-            <BinaryTreeDisplay tree={rightright}/>
-            <p>Right Right</p>
-            <BinaryTreeDisplay tree={rightleft} />
-            <p>Right Left</p>
+            <p>AVL trees use two rotation operations to rotate nodes in a tree: </p>
+            
+            
+            <div style={{ border: '2px solid black', padding: "25px", borderRadius: "10px"}}>
+                <h1>Right Rotation (node z)</h1>
+                <BinaryTreeDisplay tree={leftleft} />
+                <h2 style={{ textAlign: "center", fontSize: "50px"}}>&#129047;</h2>
+                <BinaryTreeDisplay tree={rrotation} />
+                <br />
+                <h1>Left Rotation (node z)</h1>
+                <BinaryTreeDisplay tree={rightright} />
+                <h2 style={{ textAlign: "center", fontSize: "50px" }}>&#129047;</h2>
+                <BinaryTreeDisplay tree={lrotation} />
+                <br />
+            </div>
+            <br />
+            <h3>Insertion and Deletion may result in the following patterns of nodes and thus the necessity to perform rotation operations to rebalance the tree.</h3>
+
+            <div style={{ border: '2px solid black', padding: "25px", borderRadius: "10px" }}>
+                <p style={{ textAlign: "center" }}><b>Left Left</b></p>
+                <BinaryTreeDisplay style={{ transform: "translate(100px, 100px)" }} tree={leftleft} />
+                <p style={{ textAlign: "center" }}>Resolved by a right-rotation at node z.</p>
+                
+                <br />
+                <br />
+                <p style={{ textAlign: "center" }}><b>Right Right</b></p>
+
+                <BinaryTreeDisplay tree={rightright} />
+                <p style={{ textAlign: "center" }}>Resolved by a left-rotation at node z.</p>
+                
+                <br />
+                <br />
+                <p style={{ textAlign: "center" }}><b>Right Left</b></p>
+                
+                <BinaryTreeDisplay tree={rightleft} />
+                <p style={{ textAlign: "center" }}>Resolved by a right-rotation at node y,</p>
+                <p style={{ textAlign: "center" }}>followed by a left-rotation at node z.</p>
+                <br />
+                <br />
+                <p style={{ textAlign: "center" }}><b>Left Right</b></p>
+                <BinaryTreeDisplay tree={leftright} />
+                <p style={{ textAlign: "center" }}>Resolved by a left-rotation at node y,</p>
+                <p style={{ textAlign: "center" }}>followed by a right-rotation at node z.</p>
+                
+
+                </div>
             <br />
             <table>
                 <tbody>
